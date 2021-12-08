dd419c09837eb636b2e96a4a4e284c6d4415c682
Akshay <aakshayvashishth@gmail.com>
2021-12-08 02:27:21 -0600

    Updates

---
+++ precache-manifest.b2961a54665c49826e0c2e4c4c56ef2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b5e159d2f72ce0ecb08639e0f6eca34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62632967e4e7d4e16cff",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "55d04640d987d8ce4be5",
+    "url": "/static/js/2.66d4dfe6.chunk.js"
+  },
+  {
+    "revision": "5d3d0224112a977a2f8a5568c09a46ab",
+    "url": "/static/js/2.66d4dfe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27f7d473509730d0be83",
+    "url": "/static/js/3.aa6a4637.chunk.js"
+  },
+  {
+    "revision": "62632967e4e7d4e16cff",
+    "url": "/static/js/main.45743e0d.chunk.js"
+  },
+  {
+    "revision": "175a83a8af0b2bb3513a",
+    "url": "/static/js/runtime-main.ac890416.js"
+  }
+]);
