605be0fcf2b5b89ad1c2cf18e0906909fa143b15
Akshay Sharma <57690849+akshaysharma21@users.noreply.github.com>
2023-03-28 16:12:28 -0500

    Updates

---
+++ precache-manifest.525de42dfaef81f74dabcfdba7c08482.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea98e41be5cc691c0fffb50fdc918cc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05ad49f17b5e9e8e1379",
+    "url": "/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "623fff5ec0e8a08709b8",
+    "url": "/static/js/2.052ecedc.chunk.js"
+  },
+  {
+    "revision": "ac6a8cc682f99dbbf9d106051e020a38",
+    "url": "/static/js/2.052ecedc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f56e489aeac4d2b1c51",
+    "url": "/static/js/3.70dfe1ee.chunk.js"
+  },
+  {
+    "revision": "05ad49f17b5e9e8e1379",
+    "url": "/static/js/main.e2e8f09b.chunk.js"
+  },
+  {
+    "revision": "489cf672d9f088703982",
+    "url": "/static/js/runtime-main.a0182c48.js"
+  }
+]);
