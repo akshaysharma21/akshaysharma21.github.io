8628d936b4251c89b7ac0eb3c837a2c77b49231e
Akshay Sharma <57690849+akshaysharma21@users.noreply.github.com>
2025-03-02 03:46:10 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.525de42dfaef81f74dabcfdba7c08482.js"
+  "/precache-manifest.bc7a1198429a6cba1a3ad35599b77925.js"
 );
 
 self.addEventListener('message', (event) => {
